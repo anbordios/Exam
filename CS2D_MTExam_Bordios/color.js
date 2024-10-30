823586a1095a6d702de0e95ce6d9f82ed1c87aab
anbordios <annbordios15@gmail.com>
2024-10-31 00:29:49 +0800

    color.js

---
+++ CS2D_MTExam_Bordios/color.js
@@ -0,0 +1,9 @@
+// Create an array to store three colors.
+let favColor = ["Black","White","Blue"];
+
+// Prompt the user to enter their new favorite color.
+let user = prompt("Enter your favorite color:");
+
+// Use .push() to add new color to the array.
+favColor.push("Green");
+console.log(favColor);
