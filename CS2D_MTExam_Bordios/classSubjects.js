16e6fabb5659344722f47d52f4d8eb05f0339e07
anbordios <annbordios15@gmail.com>
2024-10-30 22:00:42 +0800

    classSubjects.js

---
+++ CS2D_MTExam_Bordios/classSubjects.js
@@ -0,0 +1,11 @@
+// Declare the following user information.
+let subjectTitle = "DSA";
+let classSchedule = "8-9:30 am, MF";
+let classRoom = "IR";
+let classInstructor = "Mr. Cliff Owen Pascua";
+let studentName = "Jhury-Ann Bordios";
+let userInfo = studentName +" is currently enrolled in "+ subjectTitle +" with a class schedule of "+ 
+classSchedule +" at room "+ classRoom +". "+" The instructor for the subject is "+ classInstructor
+
+// Log all the user information to the console.
+console.log(userInfo);
