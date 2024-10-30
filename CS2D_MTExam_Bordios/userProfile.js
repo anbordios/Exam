658b7c7f7b83b5d6e1691381ac6e03c282bcd49a
anbordios <annbordios15@gmail.com>
2024-10-30 21:46:14 +0800

    userProfile.js

---
+++ CS2D_MTExam_Bordios/userProfile.js
@@ -0,0 +1,11 @@
+// Store the User Information Collections using variables.
+let fullName = "Jhury-Ann Bordios";
+let age = 20;
+let favNumber = 15;
+let favColor = "Blue";
+
+// Log all the following User Information to the console.
+console.log(fullName);
+console.log(age);
+console.log(favNumber);
+console.log(favColor);
