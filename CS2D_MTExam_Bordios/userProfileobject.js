8b92b6b5a5150f56893b5b90d9e8eedd59a19298
anbordios <annbordios15@gmail.com>
2024-10-31 08:20:07 +0800

    userProfileobject.js

---
+++ CS2D_MTExam_Bordios/userProfileobject.js
@@ -0,0 +1,10 @@
+// Create an object to store the user's information.
+let userProfile = {
+    fullName: "Jhury-Ann Bordios",
+    age: 20,
+    favoriteNumber: 7,
+    favoriteColor: "blue", 
+};
+
+// Log all the following user information inside the object.
+console.log(userProfile);
