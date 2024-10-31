9c4026ad0cff2893b1484ccced960dec90d5717c
anbordios <annbordios15@gmail.com>
2024-10-31 17:47:08 +0800

    studentEnrollment.js

---
+++ CS2D_MTExam_Bordios/studentEnrollment.js
@@ -0,0 +1,15 @@
+// Create an array to store the values.
+let sub = ["DSA", "PL", "Networks"];
+let choice1 = ["DSA"];
+let choice2 = ["PL"];
+let choice3 = ["Networks"];
+
+let subject = prompt("Select a subject:\nA.DSA\nB.PL\nC.Networks"); // Use a prompt to select the subject.
+let user = prompt("Select:\nA.Enroll\nB.Unenroll\nC.SelectAnotherSubject\nD.Exit");
+let enroll = prompt("Enter your name:"); // Prompt the user to enter their name.
+choice1.push("Jhury-Ann Bordios"); // Use .push() to enter new name in the array.
+console.log(choice1);
+choice2.push("Andrea Salem");
+console.log(choice2);
+choice3.push("Patrick Punzal");
+console.log(choice3);
